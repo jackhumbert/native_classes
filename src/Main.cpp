ad3b3f5d7e4511e67248acd62843c360140bcaf8
Jack Humbert <jack.humb@gmail.com>
2021-11-03 16:53:13 -0400

    added class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,53 +1,69 @@
 #include <RED4ext/RED4ext.hpp>
+#include <RED4ext/Scripting/IScriptable.hpp>
+#include <RED4ext/RTTITypes.hpp>
+
+struct MyCustomClass : RED4ext::IScriptable
+{
+    RED4ext::CClass* GetNativeType();
+};
+
+RED4ext::TTypedClass<MyCustomClass> cls("MyCustomClass");
+
+RED4ext::CClass* MyCustomClass::GetNativeType()
+{
+    return &cls;
+}
+
+RED4EXT_C_EXPORT void RED4EXT_CALL RegisterTypes()
+{
+    cls.flags = {.isNative = true};
+    RED4ext::CRTTISystem::Get()->RegisterType(&cls, 100000);
+}
+
+RED4EXT_C_EXPORT void RED4EXT_CALL PostRegisterTypes()
+{
+   auto rtti = RED4ext::CRTTISystem::Get();
+   auto scriptable = rtti->GetClass("IScriptable");
+   cls.parent = scriptable;
+}
+
+BOOL APIENTRY DllMain(HMODULE aModule, DWORD aReason, LPVOID aReserved)
+{
+    switch (aReason)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+        DisableThreadLibraryCalls(aModule);
+        RED4ext::RTTIRegistrator::Add(RegisterTypes, PostRegisterTypes);
+        break;
+    }
+    case DLL_PROCESS_DETACH:
+    {
+        break;
+    }
+    }
+
+    return TRUE;
+}
 
 RED4EXT_C_EXPORT bool RED4EXT_CALL Load(RED4ext::PluginHandle aHandle, const RED4ext::IRED4ext* aInterface)
 {
-    /*
-     * Here you can register your custom functions, initalize variable, create hooks and so on. All hooks created here
-     * will be attached automatically later, you do not need to attach them manually.
-     *
-     * Be sure to store the plugin handle and the interface because you cannot get it again later. The plugin handle is
-     * what identify your plugin through the extender.
-     *
-     * Returning "true" in this function loads the plugin, returning "false" will unload it and "Unload" will be called.
-     */
     return true;
 }
 
 RED4EXT_C_EXPORT void RED4EXT_CALL PostLoad()
 {
-    /*
-     * This function is called after all plugins are loaded. Use this to get interfaces to plugins you depends on.
-     */
+
 }
 
 RED4EXT_C_EXPORT void RED4EXT_CALL Unload()
 {
-    /*
-     * Here you can free resources you allocated during initalization or during the time your plugin was executed.
-     *
-     * Tis function is called when the game is closed or when "Load" returns false.
-     */
 }
 
 RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo* aInfo)
 {
-    /*
-     * This function supply the necessary information about your plugin, like name, version, support runtime and SDK. DO
-     * NOT do anything here yet!
-     *
-     * You MUST have this function!
-     *
-     * Make sure to fill all of the fields here in order to load your plugin correctly.
-     *
-     * Runtime version is the game's version, it is best to let it set to "RED4EXT_RUNTIME_LATEST" if you want to target
-     * the latest game's version that the SDK defined, if the runtime version specified here and the game's version do
-     * not match, your plugin will not be loaded. If you want to use RED4ext only as a loader and you do not care about
-     * game's version use "RED4EXT_RUNTIME_INDEPENDENT".
-     */
-
-    aInfo->name = L"RED4ext.Example.VisualStudio";
-    aInfo->author = L"WopsS";
+    aInfo->name = L"Native Classes";
+    aInfo->author = L"Jack Humbert";
     aInfo->version = RED4EXT_SEMVER(1, 0, 0);
     aInfo->runtime = RED4EXT_RUNTIME_LATEST;
     aInfo->sdk = RED4EXT_SDK_LATEST;
@@ -55,9 +71,5 @@ RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo* aInfo)
 
 RED4EXT_C_EXPORT uint32_t RED4EXT_CALL Supports()
 {
-    /*
-     * This functions returns only what API version is support by your plugins.
-     * You MUST have this function!
-     */
     return RED4EXT_API_VERSION_LATEST;
 }
